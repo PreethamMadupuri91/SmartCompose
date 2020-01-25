f306cfbc3c15d3ae8674bcfb93e96aab4aed21ea
Preetham <preetham@Preethams-MacBook-Air.local>
2020-01-25 21:14:06 +0530

    Initial commit

---
+++ Solution.py
@@ -0,0 +1,78 @@
+'''
+GMAIL SMART COMPOSE APPLICATION
+START DATE : Jan 23, 2020
+END DATE : Jan 25, 2020
+RNN Model : One-to-Many words
+CHARACTER LEVEL Model
+ONE-TO-MANY Model
+KERAS Uses THEANO
+'''
+
+from numpy import array
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+
+# generate a sequence from the model
+def generate_seq(model, tokenizer, seed_text, n_words):
+	in_text, result = seed_text, seed_text
+	# generate a fixed number of words
+	for _ in range(n_words):
+		# encode the text as integer
+		encoded = tokenizer.texts_to_sequences([in_text])[0]
+		encoded = array(encoded)
+		# predict a word in the vocabulary
+		yhat = model.predict_classes(encoded, verbose=0)
+		# map predicted word index to word
+		out_word = ''
+		for word, index in tokenizer.word_index.items():
+			if index == yhat:
+				out_word = word
+				break
+		# append to input
+		in_text, result = out_word, result + ' ' + out_word
+	return result
+
+data_file = "/Users/preethamkumarmadupuri/PycharmProjects/smartcompose/Data/smsspamcollection/SMSSpamCollection.txt"
+
+#data= "How are you?: \n How are you?: \n How do you do?"
+
+with open(data_file, 'r') as data:
+    data = data.read().replace('\n', '')
+
+
+# integer encode text
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts([data])
+encoded = tokenizer.texts_to_sequences([data])[0]
+# determine the vocabulary size
+vocab_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d' % vocab_size)
+# create word -> word sequences
+sequences = list()
+for i in range(1, len(encoded)):
+	sequence = encoded[i-1:i+1]
+	sequences.append(sequence)
+print('Total Sequences: %d' % len(sequences))
+# split into X and y elements
+sequences = array(sequences)
+X, y = sequences[:,0],sequences[:,1]
+# one hot encode outputs
+y = to_categorical(y, num_classes=vocab_size)
+# define model
+model = Sequential()
+model.add(Embedding(vocab_size, 10, input_length=1))
+model.add(LSTM(50))
+model.add(Dense(vocab_size, activation='softmax'))
+print(model.summary())
+# compile network
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit network
+model.fit(X, y, epochs=10, verbose=2)
+# evaluate
+print("\n Welcome to Smart Compose App: \n")
+print("Input : How ")
+print("Output : " +generate_seq(model, tokenizer, 'How', 2))
